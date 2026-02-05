5be72a4c81c4894b284db57e1e591b88bd2c24e0
KittitatK <skhoogkhogsung@gmail.com>
2026-02-05 09:00:55 +0700

    ผมเป็นผู้ชาย

---
+++ Puchaychamp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Puchaychamp {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        
+        System.out.println("แชมป์เป็นผู้ชายตัวจริงเสียงจริง");
+        //เพื่อนผมเป็นเพศอะไร
+    }
+}
