f16257e6badb04781d66ae9af63c405bde12f375
Aungkanr <augkar1234@gmail.com>
2026-02-05 09:47:49 +0700

    test merge conflict

--- JavaCode/Class.java
+++ JavaCode/Class.java
@@ -5,16 +5,20 @@ public class Class {
     private String ID;
     private String Name;
     private int Age;
+    private int state;
 
-    public Class ( String idnakren , String Namenakren , int Ayunakren ) {
+    public Class ( String idnakren , String Namenakren , int Ayunakren , int classren ) {
     
         ID = idnakren ;
         Name = Namenakren;
         Age = Ayunakren;
+        state = classren;
+
     
         System.out.println("idnakren : " + ID );
         System.out.println("Namenakren : " + Name );
         System.out.println("Ayunakren : " + Age );
+        System.out.println("cless : " + state );
         
     }
 }
